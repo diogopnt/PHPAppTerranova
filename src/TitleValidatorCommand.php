8d456f9dfc1b5fba95a21f60b6b8919584fe4a69
diogopnt <pintodiogo@ua.pt>
2024-03-19 10:58:35 +0000

    Filtagrem de url´s válidos no comando de comparação de titulos

--- src/TitleValidatorCommand.php
+++ src/TitleValidatorCommand.php
@@ -48,25 +48,51 @@ class TitleValidatorCommand extends Command
             echo "\n" . $pageTitle . PHP_EOL . "\n" . $url . "\n";
             //echo $title . "\n";
         }
+
+        return Command::SUCCESS;
     }
 
 
     protected static function checkURLPageTitle($url)
     {
 
-        $html = file_get_contents($url);
-        preg_match("/<title>(.+)<\/title>/i", $html, $title);
-        preg_match_all('/<meta .*?name=["\']?([^"\']+)["\']? .*?content=["\']([^"\']+)["\'].*?>/i', $html, $meta);
+        $status = self::checkUrlStatus($url);
 
-        return "Title: " . $title[1];
-        /*
-        for ($i = 0; $i < count($meta[1]); $i++) {
-            return "Meta " . $meta[1][$i] . ": " . $meta[2][$i] . "<br>";
-        }*/
+        if ($status == "URL não encontrada (status 404)") {
+            return "Página não encontrada";
+        } elseif ($status == "URL encontrada") {
+            $html = file_get_contents($url);
+            preg_match("/<title>(.+)<\/title>/i", $html, $title);
+            preg_match_all('/<meta .*?name=["\']?([^"\']+)["\']? .*?content=["\']([^"\']+)["\'].*?>/i', $html, $meta);
+
+            return "Title: " . $title[1];
+            /*
+            for ($i = 0; $i < count($meta[1]); $i++) {
+                return "Meta " . $meta[1][$i] . ": " . $meta[2][$i] . "<br>";
+            }*/
+        }
+    }
+
+    protected static function checkUrlStatus($url)
+    {
+        $headers = get_headers($url);
+
+        if ($headers === false) {
+            return "Erro ao obter os cabeçalhos HTTP";
+        }
+
+        $statusCode = explode(' ', $headers[0])[1];
+
+        if ($statusCode == '200' || $statusCode == '302') {
+            return "URL encontrada";
+        } elseif ($statusCode == '404') {
+            return "URL não encontrada (status 404)";
+        } else {
+            return "Código de status HTTP desconhecido: $statusCode";
+        }
     }
 
     protected static function comparePageTitle($title, $urlPageTitle)
     {
-
     }
 }
